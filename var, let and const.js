cf2bf65957a483273bf08ce96f96721344899d0b
Rao Fahad <raofahad046@gmail.com>
2020-11-15 17:11:55 +0500

    added var,let and const.js and modified index.html

---
+++ var, let and const.js
@@ -0,0 +1,67 @@
+//  var is a global scope and let is a block scope
+
+// var someVar = 'rao fahad';
+// let newVar = 'abc';
+
+
+// var newVar = 'rao';
+// var times = 4;
+// if (times > 3){
+//     var newVar = 'fahad';
+//     console.log('inside if block',newVar);
+// }
+// console.log('outside if block', newVar);
+
+
+
+// var newVar = 'rao';
+// var times = 4;
+// if ( times > 3){
+//     let newVar = 'fahad';
+//     console.log('inside if block', newVar);
+// }
+// console.log('outside if block', newVar);
+
+
+
+// var times = 4;
+// if ( times > 3){
+//     let newVar = 'fahad';
+//     console.log('inside if block', newVar);
+// }
+// console.log('outside if block', newVar);
+
+
+
+// let myname; //declare variable
+// myname = 'rao';  //initialize variable
+// console.log(myname);  //result: rao
+
+
+
+// let myname; //declare variable
+// console.log(myname);  //result: undefined
+// myname = 'fahad';  // initialize variable
+
+
+
+//  excellent article on let const variable
+// https://andy-carter.com/blog/variable-scope-in-modern-javascript
+
+
+
+// let firstVar = 'rao';
+// const secondVar = 'fahad';
+// firstVar = 'asim';
+// secondVar = 'khan';
+// console.log('firstVariable', firstVar);
+// console.log('secondVariable', secondVar);
+
+
+
+// let firstVar = 'rao';
+// const secondVar = 'fahad';
+// firstVar = 'asim';
+// console.log('first variable', firstVar);
+// secondVar = 'khan';
+// console.log('second variable', secondVar);
