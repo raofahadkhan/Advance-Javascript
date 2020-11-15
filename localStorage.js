2eb5b3cb292f67c5928872677909a8c5b7eb9901
Rao Fahad <raofahad046@gmail.com>
2020-11-16 04:06:24 +0500

    added localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,24 @@
+// localStorage.setItem('fname','Rao');
+// localStorage.setItem('lname', 'Fahad');
+
+// let firstName = localStorage.getItem('fname');
+// let lastName = localStorage.getItem('lname');
+// console.log(firstName, lastName);
+
+// localStorage.removeItem('fname');
+// localStorage.clear()
+
+
+
+// HOW TO STORE OBJECTS IN LOCAL STORAGE IN HTML 5:
+
+// let myInfo = { name: 'fahad rao', age: 21, married: false };
+// localStorage.setItem('bio', JSON.stringify(myInfo));
+
+// let retObj = JSON.parse(localStorage.getItem('bio'));
+
+// console.log(retObj);
+// console.log(retObj.name, retObj.age, retObj.married);
+// console.log(localStorage.length);
+// localStorage.clear();
+
