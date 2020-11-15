569d91899d866ffc28320a83d6f4ad378fadbfab
Rao Fahad <raofahad046@gmail.com>
2020-11-15 17:25:15 +0500

    added primitive and non primitive datatypes.js and modified index.html

---
+++ Primitive and non-primitive datatypes.js
@@ -0,0 +1,26 @@
+// datatypes: primitive and non-primitive
+// primitive : Number, string, boolean, null, undefined, symbol
+// non-primitive: objects
+
+// primitive datatypes are immutable, they are passed by values
+// non primitive datatypes are mutable, they are passed by ReferenceError
+
+
+// const arr = ['rao', 'fahad'];
+// arr[0] = 'asim';
+// console.log('result',arr);
+
+
+
+// const obj = {
+//     name: 'raofahadkhan',
+//     age: 21
+// }
+// obj.age = 23;
+// console.log(obj.age);
+
+
+// let name = 'raofahadkhan';   //['r','a','o','f','a','h',...]
+// console.log(name[2]);
+// name[2] = O;
+// console.log(name);  //it doesnt change because they are immutable
