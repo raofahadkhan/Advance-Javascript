d3d2f86fd585b9b919c129779a8547130714b086
Rao Fahad <raofahad046@gmail.com>
2020-11-16 03:27:05 +0500

    added map.js and modified index.html

---
+++ map.js
@@ -0,0 +1,25 @@
+// const items = [
+//     { name: 'Bike', price: 100 },
+//     { name: 'TV', price: 200 },
+//     { name: 'Album', price: 10 },
+//     { name: 'Book', price: 5 },
+//     { name: 'Phone', price: 2000 },
+//     { name: 'Keyboard', price: 50 },
+//     { name: 'Mouse', price: 500 },
+//     { name: 'Computer', price: 30 },
+// ];
+
+
+// OLD WAY OF DOING THE SAME WORK AS MAP DOES
+// const mapArray = [];
+// for(let i = 0; i < items.length; i++){
+//     mapArray.push(items[i].name);
+// }
+// console.log(mapArray);
+
+
+// const mapArray = items.map(element => console.log(element));
+
+
+// const mapArray = items.map((element, index) => element.name )
+// console.log(mapArray);
