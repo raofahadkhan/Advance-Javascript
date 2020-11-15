fc4af40c49ea2c81aaca2560c04cf7c83094ecad
Rao Fahad <raofahad046@gmail.com>
2020-11-16 03:47:59 +0500

    added find.js

---
+++ find.js
@@ -0,0 +1,23 @@
+//  const items = [
+//         { name: 'Bike', price: 100 },
+//         { name: 'TV', price: 200 },
+//         { name: 'Album', price: 10 },
+//         { name: 'Book', price: 5 },
+//         { name: 'Phone', price: 2000 },
+//         { name: 'Keyboard', price: 50 },
+//         { name: 'Mouse', price: 500 },
+//         { name: 'Computer', price: 30 },
+//         { name: 'Car', price: 5000 },
+        
+//     ];
+    
+
+// FIND RETURNS AN OBJECT 
+// AND STOP WHEN IT FIND THE FIRST POSSIBLE CONDITION AND WHEN
+// THE CONDITION DOESNT SATISFY IT THROWS UNDEFINED
+
+// const foundItem = items.find((item,index) => {
+//     return item.price <= 2000;
+// })
+
+// console.log(foundItem)
