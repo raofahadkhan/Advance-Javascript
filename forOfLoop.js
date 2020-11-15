2a07067a37936f9828dab03510681b620e4d1514
Rao Fahad <raofahad046@gmail.com>
2020-11-16 02:42:17 +0500

    added forOfLoop.js and modified index.html

---
+++ forOfLoop.js
@@ -0,0 +1,10 @@
+// const iterable = [10, 20, 30];
+// for(let number of iterable){
+//     console.log(number);
+// }
+
+// const myname = 'rao';
+// for(const alphabet of myname){
+//     console.log(alphabet);
+// }
+
