d5d4e3f1b022697ff1b4b5b824bae145027126c9
Rao Fahad <raofahad046@gmail.com>
2020-11-16 02:28:49 +0500

    added tryCatch.js and modified index.html

---
+++ tryCatch.js
@@ -0,0 +1,40 @@
+// try{
+//     aler('helloworld');
+// }catch(error){
+//     console.log('error',error);
+// }
+
+
+// const a = 'string';
+// try{
+//     if(typeof(a) === 'string'){
+//         throw 'something went wrong we expect integer'
+//     }
+// }catch(error){
+//     console.log('error', error);
+// }
+
+
+// try{
+//     const age = 20;
+//     if(age < 18){
+//         throw 'you are eligible';
+//     }else{
+//         alert('you are eligible');
+//     }
+// }catch(error){
+//     console.log('error message',error);
+// }
+
+
+
+// try{
+//     const a = 58;
+//     if(typeof(a) === 'string'){
+//         alert('this is an string');
+//     }else{
+//         throw 'we expect string instead of other data type';
+//     }
+// }catch(err){
+//     console.log('there is some problem ' + err);
+// }
