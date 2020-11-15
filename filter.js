d1732a3bfb8a76adfc3d3f13ffbbec049fac5c66
Rao Fahad <raofahad046@gmail.com>
2020-11-16 03:36:03 +0500

    added filter.js and modified index.html

---
+++ filter.js
@@ -0,0 +1,26 @@
+// FILTER WORKS ON CONDITION
+
+const items = [
+        { name: 'Bike', price: 100 },
+        { name: 'TV', price: 200 },
+        { name: 'Album', price: 10 },
+        { name: 'Book', price: 5 },
+        { name: 'Phone', price: 2000 },
+        { name: 'Keyboard', price: 50 },
+        { name: 'Mouse', price: 500 },
+        { name: 'Computer', price: 30 },
+        { name: 'Car', price: 5000 },
+        
+    ];
+    
+//  const filterArray = items.filter((elem, index) => elem.price <= 100);
+//  console.log(filterArray);
+
+
+// const filterArray = items.map((elem, index) => {
+//     return elem.price <=100;
+// })
+// console.log(filterArray);
+
+
+// MAP SHOWS OUTPUT IN TRUE FALSE IN THIS CASE
