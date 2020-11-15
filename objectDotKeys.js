fcf5f2c796f35308bbd79ffda007a53d921c1bfe
Rao Fahad <raofahad046@gmail.com>
2020-11-16 04:43:15 +0500

    added objectDotKeys.js

---
+++ objectDotKeys.js
@@ -0,0 +1,15 @@
+// let myObj = {
+//     name: 'raofahadkhan',
+//     city: 'khi',
+//     address: {
+//         address: 'one'
+//     }
+// }
+
+// let objKeys = Object.keys(myObj);
+// console.log(objKeys);
+
+// for (let index = 0; index < objKeys.length; index++) {
+//     console.log(objKeys[index],':',myObj[objKeys[index]])
+    
+// }
