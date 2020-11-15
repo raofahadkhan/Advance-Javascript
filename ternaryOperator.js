87c3ea7f0cc432025c2c62bf08ad2650a3252656
Rao Fahad <raofahad046@gmail.com>
2020-11-15 19:38:31 +0500

    added ternaryOperator.js and modified index.html

---
+++ ternaryOperator.js
@@ -0,0 +1,67 @@
+// const a = 100;
+// const b = 20;
+
+// syntax: condition ? true : false
+
+// a < b ? console.log('if is running') : console.log('else is running');
+
+// a > b  
+//     ? a === 12
+//         ? console.log('nested of running')
+//         : console.log('nested else running')
+//     : console.log('parent else running')    
+
+
+
+
+// let age = 19;
+// let canDrive;
+
+// age > 16 ? canDrive = 'yes' : canDrive = 'no'
+
+// age > 16
+//       ? canDrive = 'yes'
+//       : canDrive = 'no'
+
+// console.log(canDrive)
+
+
+
+
+// function foo(bar){
+//     bar = typeof(bar) !== 'undefined' ? bar : 10;
+//     console.log(bar);
+// }
+// foo();
+// foo(25);
+
+
+
+
+// var authenticated = false;
+// var nextURL = authenticated ? (
+//     alert('you will redirect ot admin area'),
+//     '/admin'
+// ) : (
+//     alert('Access denied'),
+//     '/403'
+// );
+
+// console.log(nextURL); 
+
+
+
+// var locked = 1;
+// var canChange = locked !== 1 ? true : false;
+// console.log(canChange);
+
+
+// var locked = 1;
+// var canChange = locked != 1;
+// console.log(canChange);
+
+
+
+// var speed = 90;
+// var message = speed >= 120 ? 'too fast' : (speed >= 80 ? 'fast' : 'ok');
+// console.log(message);
