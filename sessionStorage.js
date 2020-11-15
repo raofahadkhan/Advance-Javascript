0c564b8869707415830548ed9a447d9cfa60c070
Rao Fahad <raofahad046@gmail.com>
2020-11-16 04:17:17 +0500

    added sessionStorage.js

---
+++ sessionStorage.js
@@ -0,0 +1,15 @@
+// sessionStorage.setItem('fname','rao');
+// sessionStorage.setItem('lname','fahad');
+// let firstName = sessionStorage.getItem('fname');
+// let lastName = sessionStorage.getItem('lname');
+// sessionStorage.removeItem('fname');
+// console.log(firstName);
+// sessionStorage.clear();
+
+// HOW TO STORE OBJECTS IN SESSION STORAGE IN HTML 5:
+
+// let myInfo = { name: 'fahad rao', age: 21, married: false };
+// sessionStorage.setItem('bio', JSON.stringify(myInfo));
+// let info = JSON.parse(sessionStorage.getItem('bio'));
+// console.log(info.name, info.age, info.married);
+// sessionStorage.clear();
