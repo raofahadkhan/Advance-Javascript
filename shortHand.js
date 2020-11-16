634af8a25216f3d8d0f7baf9d948c9e71293e762
Rao Fahad <raofahad046@gmail.com>
2020-11-16 05:15:36 +0500

    added shortHand.js

---
+++ shortHand.js
@@ -0,0 +1,19 @@
+// it doesnt make true objects 
+// it just passes the reference of the variables
+
+// let firstName = 'rao';
+// let lastName = 'fahad';
+
+// const myObj = {
+//     firstName: firstName,
+//     lastName: lastName
+// }
+// console.log(myObj)
+
+// new way
+
+// const anotherObj = {
+//     firstName,
+//     lastName
+// }
+// console.log(anotherObj);
