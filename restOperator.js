40721b3ca0a50213e6a9fe9e135c9f4ccc7bec98
Rao Fahad <raofahad046@gmail.com>
2020-11-16 05:51:47 +0500

    added restOperator.js

---
+++ restOperator.js
@@ -0,0 +1,37 @@
+// let obj = {
+//     firstName: 'rao',
+//     lastName: 'fahad',
+//     city: 'khi'
+// }
+// console.log(obj);
+
+// let { firstName, ...restValues} = obj;
+// console.log(firstName, restValues);
+
+// let { lastName, ...restValues } = obj;
+// console.log(lastName, restValues);
+
+// let { anotherValue, ...restValue} = obj;
+// console.log(anotherValue, restValue);
+
+
+// let obj = {
+//     firstName: 'rao',
+//     lastName: 'fahad',
+//     city: {
+//         nameOne: 'khi',
+//         nameTwo: 'lahore'
+//     }
+// }
+
+// let { firstName, ...restval } = obj;
+// console.log(firstName, restval);
+
+
+// let myFunc = (...args) => {
+//     return args.reduce((previous, current) => {
+//         return previous + current;
+//     });
+// }
+// console.log(myFunc(1,25,15,3,9,25,43,6,22));
+
