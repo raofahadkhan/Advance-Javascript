62abdde9c0625ac1959bf1d9a93badb368bc1d8a
Rao Fahad <raofahad046@gmail.com>
2020-11-16 15:59:47 +0500

    added async.js

---
+++ async.js
@@ -0,0 +1,10 @@
+// const name = 'fahad';
+// setTimeout(() => console.log('this is set time out'),2000);
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//       .catch(err => console.log('error',err));
+//       console.log(name) 
+
+// to chech catch statement minipulate the endpoint
