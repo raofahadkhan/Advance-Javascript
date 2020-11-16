d8a978376aef1fab73f3bfeb2064c968266868a7
Rao Fahad <raofahad046@gmail.com>
2020-11-16 06:07:19 +0500

    added sets.js

---
+++ sets.js
@@ -0,0 +1,24 @@
+// const newSet = new Set(['rao','fahad','khan','fahad']);
+// console.log(newSet);
+
+// const newSet = new Set();
+// newSet.add('fahad');
+// newSet.add('fahad');
+// newSet.add('rao');
+// newSet.add('saqlain');
+// console.log(newSet);
+// newSet.delete('rao');
+// console.log(newSet);
+
+// const newSet = new Set();
+// newSet.add('fahad');
+// newSet.add('fahad');
+// newSet.add('rao');
+// newSet.add('saqlain');
+
+// sets are iterable
+
+// for(i of newSet){
+//     console.log(i);
+// }
+
