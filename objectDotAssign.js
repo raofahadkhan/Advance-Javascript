d602a1ddefed229bdd34a10f542a488f26b6d1df
Rao Fahad <raofahad046@gmail.com>
2020-11-16 05:09:39 +0500

    added objectDotAssign

---
+++ objectDotAssign.js
@@ -0,0 +1,29 @@
+let myObj = {
+    firstName: 'rao'
+}
+
+let myObj2 = {
+    lastName: 'fahad'
+}
+
+// let finalObj = {
+//     ...myObj,
+//     ...myObj2
+// }
+// console.log(finalObj);
+
+// let anotherFinalObj = Object.assign({}, myObj, myObj2);
+// anotherFinalObj.firstName = 'ismail'; // checking that is a true copy or not
+// myObj.firstName = 'asim';
+// console.log(anotherFinalObj);
+// console.log(myObj);
+
+// let newObj = Object.assign(myObj, myObj2);
+// myObj2.lastName = 'haseeb';
+// myObj.firstName = 'fawad';
+// console.log('myobj', myObj);
+// console.log('myObj2', myObj2);
+// console.log('newobj', newObj);
+
+// WHEN WE CHANGES A VALUE OF KEY IN TARGET OBJECT IT WILL CHANGE THE
+// FINAL OBJECT AS WELL
