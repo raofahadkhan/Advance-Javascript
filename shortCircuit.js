5ee91799d206e17c8596817223a33f16c1e8264c
Rao Fahad <raofahad046@gmail.com>
2020-11-16 15:47:01 +0500

    added shortCircuit.js

---
+++ shortCircuit.js
@@ -0,0 +1,27 @@
+// let gender = 'male';
+// let msg = (gender === 'male') && '&& short circuit is working';
+// ---in this short circuit when the first condition gets
+// ---true it will run the condition which is next to &&
+// console.log(msg);
+
+
+// let gender = 'male';
+// let msg2 = (gender === 'female') || '|| short circuit works';
+// ---in this short circuit when the first condition gets true it will
+// ---store that value in it
+// console.log(msg2);
+
+// var person = {
+//     name: 'rao',
+//     age: 21
+// }
+// console.log(person.job || 'unemployed');
+
+
+// var a;
+// var b = null;
+// var c = undefined;
+// var d = 4;
+// var e = 'five';
+// var f = a || b || c || d || e;
+// console.log(f);
