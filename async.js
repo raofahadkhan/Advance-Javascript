8db5faf6903a4b7d794db377558188f087043038
Rao Fahad <raofahad046@gmail.com>
2020-11-18 03:03:29 +0500

    modified async.js

--- async.js
+++ async.js
@@ -8,3 +8,26 @@
 //       console.log(name) 
 
 // to chech catch statement minipulate the endpoint
+
+const posts = [
+    {title: 'post one', body: 'this is post one'},
+    {title: 'post two', body: 'this is post two'}
+];
+// map returns array but forEach doesnt
+function getPosts(){
+    let output = '';
+    setTimeout(() => {
+        posts.forEach((post) => { //we can do this task with map also
+            output += `<li>${post.title}</li>`
+            document.body.innerHTML = output;
+        })
+    }, 1000);
+}
+function createPost(post, callback){
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+createPost({title: 'post three', body: 'this is post three'}, getPosts);
+//when we passes function as an arguement we dont put paranthesis with it
