575ab1d62c6a0a29e3af23c750cb5b8ea40266c2
Rao Fahad <raofahad046@gmail.com>
2020-12-06 00:43:35 +0500

    added post api call in asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,25 +1,44 @@
-const getPosts = async () => {
+// const getPosts = async () => {
 
-    try {
-        const postResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const userResponse = await fetch('https://jsonplaceholder.typicode.com/users');
-        const posts = await postResponse.json();
-        const postsTitle = posts.map((post) => {
-            return {
-                title: post.title
-            }
-        });
-        const users = await userResponse.json();
-        // console.log('response ==========>', posts);
-        // console.log('response user =====>', users);
-        console.log(postsTitle);
-    } catch (error) {
-        console.log(error);
-    }
+//     try {
+//         const postResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         const userResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const posts = await postResponse.json();
+//         const postsTitle = posts.map((post) => {
+//             return {
+//                 title: post.title
+//             }
+//         });
+//         const users = await userResponse.json();
+//         // console.log('response ==========>', posts);
+//         // console.log('response user =====>', users);
+//         console.log(postsTitle);
+//     } catch (error) {
+//         console.log(error);
+//     }
 
+// }
+
+// getPosts();
+
+// console.log('outside getposts function');
+
+async function postData(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify({
+            title: "My Info",
+            body: "Data",
+            userId: 4
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+    console.log("response = ", response);
+
+    const data = await response.json();
+    console.log("Data = ", data);
 }
 
-getPosts();
-
-console.log('outside getposts function');
-
+postData();
